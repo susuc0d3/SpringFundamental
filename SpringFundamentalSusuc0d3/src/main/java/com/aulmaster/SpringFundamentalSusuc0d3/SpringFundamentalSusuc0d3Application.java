7fd029e9bd8e105633933ff3fd9c4046ab0d22b9
KOPIHITAM\KOPI <susuc0d3@gmail.com>
2022-11-08 00:22:09 +0700

    nambah project baru

---
+++ SpringFundamentalSusuc0d3/src/main/java/com/aulmaster/SpringFundamentalSusuc0d3/SpringFundamentalSusuc0d3Application.java
@@ -0,0 +1,13 @@
+package com.aulmaster.SpringFundamentalSusuc0d3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFundamentalSusuc0d3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFundamentalSusuc0d3Application.class, args);
+	}
+
+}
---
+++ SpringFundamentalSusuc0d3/src/test/java/com/aulmaster/SpringFundamentalSusuc0d3/SpringFundamentalSusuc0d3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulmaster.SpringFundamentalSusuc0d3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFundamentalSusuc0d3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
