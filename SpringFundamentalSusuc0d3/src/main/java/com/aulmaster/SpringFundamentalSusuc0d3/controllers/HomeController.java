8a1e583fe4799190fd6ed65b99f4acac6a4240ef
KOPIHITAM\KOPI <susuc0d3@gmail.com>
2022-11-08 01:47:46 +0700

    nambahin template

---
+++ SpringFundamentalSusuc0d3/src/main/java/com/aulmaster/SpringFundamentalSusuc0d3/controllers/HomeController.java
@@ -0,0 +1,24 @@
+package com.aulmaster.SpringFundamentalSusuc0d3.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("")
+    public String rootIndex(){
+        return "redirect:/susuc0d3";
+    }
+
+    @RequestMapping(value = "/susuc0d3", method = RequestMethod.GET)
+    @GetMapping("")
+    public String homeIndex(){
+        return "layout/adminbite/master";
+    }
+}
